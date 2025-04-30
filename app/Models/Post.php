013f073034a792772ccef76c621f8ad8131e358e
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-30 12:55:57 +0100

    blog-cms

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,10 +1,21 @@
 <?php
 
+<?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'slug', 'content', 'user_id', 'status', 'published_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
